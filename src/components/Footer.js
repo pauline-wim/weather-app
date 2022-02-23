29c400d074779aa173f8d77040932fdd7278e24f
pauline-wim <linette.wim@hotmail.com>
2022-02-23 22:38:04 +0100

    Change Style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,14 @@ import dayjs from "dayjs";
 import styled from "styled-components";
 
 function Footer() {
-  const date = dayjs().format("MMMM DD, YYYY");
+  const date = dayjs().format("· MMMM DD, YYYY ·");
   const hour = dayjs().format("HH:mm");
   return (
     <FooterSection>
       <h2>Today's weather...</h2>
       <div className="time">
         <h2>{date}</h2>
-        <h2>{hour}</h2>
+        <h3>{hour}</h3>
       </div>
       <p>© 2022. Pauline Wimmer.</p>
     </FooterSection>
@@ -21,7 +21,7 @@ function Footer() {
 const FooterSection = styled.footer`
   display: flex;
   justify-content: space-between;
-  align-items: center;
+  align-items: baseline;
   padding: 30px;
   nav {
     /* border: 2px solid red; */
