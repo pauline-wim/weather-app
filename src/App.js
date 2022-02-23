c358a6cbc8a24e3581156384fd11820b57f42476
pauline-wim <linette.wim@hotmail.com>
2022-02-23 11:35:40 +0100

    Add components & pages to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+// PAGES
+import Home from "./pages/Home";
+import Fav from "./pages/Fav";
+// COMPONENTS
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+// CSS
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div>
+        <h1>What's the Weather like Today?</h1>
+      </div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/fav" component={Fav} />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
